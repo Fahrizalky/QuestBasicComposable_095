b22aaad5d4604ef4b2e9738dd742a4b942ccb0f1
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-10-18 16:33:33 +0700

    mengubah border foto

--- app/src/main/java/com/example/projectempty/MainActivity.kt
+++ app/src/main/java/com/example/projectempty/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -95,7 +101,11 @@ fun BasicCompose(
             fontSize = 45.sp
         )
 val sizeFoto = Modifier
-    .size(600.dp)
+    .width(250.dp)
+    .height(400.dp)
+    .clip(RoundedCornerShape(250.dp))
+    .border(5.dp, Color.Gray)
+
 
         Image(
             painter = painterResource(id = R.drawable.kmti),
